7a6468c74c3c244aed765428736ca6cd8e2e696f
Arvin <arvincalinog11@gmail.com>
2026-02-05 13:50:49 +0800

    Initial commit for new repository

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Breighton-Agent-Attendance/", // Only the repo name, wrapped in slashes
+  base: "/Breighton-Agent-Attendance/", // Must match your repo name exactly
 })
